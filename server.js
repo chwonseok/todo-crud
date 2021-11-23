f3a8dc72cf70e64a24fc8b3338590d8ad2d1f668
chwonseok <dev.chwonseok@gmail.com>
2021-11-23 10:45:14 +0900

    Add: getting data from server and push them into listEjs

--- server.js
+++ server.js
@@ -30,7 +30,15 @@ app.get('/write', (req, res) => {
 
 // Print data from DB w/EJS file (하는 중)
 app.get('/list', (req, res) => {
-  res.render('list.ejs');
+  // data 가져오기
+  // meta data제외한 모든 데이터 가져오는 식
+  db.collection('post')
+    .find()
+    .toArray((error, result) => {
+      console.log(result);
+      // 가져온 data를 포함해서 render하기
+      res.render('list.ejs', { post: result });
+    });
 });
 
 ///////////////// POST
